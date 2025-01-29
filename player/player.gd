320ddcaef99b9651a8a5d0315989250a11900149
7minutech <elijohnson1107@gmail.com>
2025-01-29 11:13:06 -0600

    :refactor: new player/enemy/world

--- player/player.gd
+++ player/player.gd
@@ -1,35 +1,46 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const speed = 100
+var current_direction = "none"
 
+func _physics_process(delta: float):
+	player_movement(delta)
+	pass
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("left", "right")
-	if direction:
-		face_player(direction)
-		$AnimatedSprite2D.play("walk")
-		velocity.x = direction * SPEED
+func _ready():
+	$AnimatedSprite2D.play("idle")
+	
+func player_movement(delta):
+	if Input.is_action_pressed("right"):
+		current_direction = "right"
+		play_anim(true)
+		velocity.x = speed
+		velocity.y = 0
+	elif  Input.is_action_pressed("left"):
+		current_direction = "left"
+		play_anim(true)
+		velocity.x = -speed
+		velocity.y = 0
 	else:
-		$AnimatedSprite2D.play("idle")
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		play_anim(false)
+		velocity.x = 0
+		velocity.y = 0
 	move_and_slide()
 
-func face_player(directionn):
-	if directionn < 0:
-		$AnimatedSprite2D.flip_h = true
-	else:
-		$AnimatedSprite2D.flip_h = false
+func play_anim(movement : bool):
+	var direction = current_direction
+	var anim = $AnimatedSprite2D
+	if direction == "right":
+		anim.flip_h = false
+		if movement:
+			anim.play("walk")
+		else:
+			anim.play("idle")
+	elif direction == "left":
+		anim.flip_h = true
+		if movement: 
+			anim.play("walk")
+		else:
+			anim.play("idle")
 	
