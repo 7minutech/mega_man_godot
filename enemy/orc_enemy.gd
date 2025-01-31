da7d75553a967c9d04aea7904cecefcc62eaea8d
7minutech <elijohnson1107@gmail.com>
2025-01-31 10:38:24 -0600

    feat: enemy chase

--- enemy/orc_enemy.gd
+++ enemy/orc_enemy.gd
@@ -1,25 +1,28 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+var speed = 100
+var player_chase  = false
+var player = null
 
-
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+func _physics_process(delta: float):
+	if(player_chase):
+		position += (player.position - position)/speed
+		$AnimatedSprite2D.play("walk")
+		if ((player.position.x - position.x)	 < 0):
+			$AnimatedSprite2D.flip_h = true
+		else:
+			$AnimatedSprite2D.flip_h = false
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		$AnimatedSprite2D.play("idle")
 
-	move_and_slide()
+func _on_dectectoin_area_body_entered(body: Node2D) -> void:
+	player = body
+	player_chase = true
+	pass # Replace with function body.
+
+
+func _on_dectectoin_area_body_exited(body: Node2D) -> void:
+	player = null
+	player_chase = false
+	pass # Replace with function body.
