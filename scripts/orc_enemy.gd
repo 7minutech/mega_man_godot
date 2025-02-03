684f55d6c25e7e329bba41c1aa5329d290a4ef18
7minutech <elijohnson1107@gmail.com>
2025-02-03 08:26:18 -0600

    feat: player can atk enemy

--- scripts/orc_enemy.gd
+++ scripts/orc_enemy.gd
@@ -6,7 +6,8 @@ var player_chase  = false
 var player = null
 var health = 100
 var player_in_atk_range = false
-
+var attack_ip = false
+var can_take_damage = true
 
 
 func _physics_process(delta: float):
@@ -31,6 +32,7 @@ func _on_dectectoin_area_body_exited(body: Node2D) -> void:
 	player = null
 	player_chase = false
 	pass # Replace with function body.
+	
 func enemy():
 	pass
 
@@ -43,12 +45,20 @@ func _on_enemy_hitbox_body_entered(body: Node2D) -> void:
 
 func _on_enemy_hitbox_body_exited(body: Node2D) -> void:
 	if(body.has_method("player")):
-		player_in_atk_range = true
+		player_in_atk_range = false
 	pass # Replace with function body.
 
 func deal_with_dmg():
-	if(player_in_atk_range && global.player_current_atk):
-		health -= 20
-		print(health)
-		if (health <= 0):
-			self.queue_free()
+	if(player_in_atk_range && Global.player_current_atk):
+		if can_take_damage:
+			health -= 20
+			$DMG_Cooldown.start()
+			can_take_damage = false
+			print(health)
+			if (health <= 0):
+				self.queue_free()
+
+
+func _on_dmg_cooldown_timeout() -> void:
+	can_take_damage = true
+	pass # Replace with function body.
--- scripts/player.gd
+++ scripts/player.gd
@@ -5,12 +5,14 @@ const speed = 100
 var current_direction = "none"
 var enemy_in_atk_range = false
 var enemy_atk_cd = true
-var health = 100
+var health = 200
 var alive = true
+var atk_ip = false
 	
 func _physics_process(delta: float):
 	player_movement(delta)
 	enemy_atk()
+	attack()
 	if health <= 0:
 		health = 0
 		# end scren here 
@@ -24,35 +26,41 @@ func _ready():
 func player_movement(delta):
 	if Input.is_action_pressed("right"):
 		current_direction = "right"
-		play_anim(true)
+		play_anim(1)
 		velocity.x = speed
 		velocity.y = 0
 	elif  Input.is_action_pressed("left"):
 		current_direction = "left"
-		play_anim(true)
+		play_anim(1)
 		velocity.x = -speed
 		velocity.y = 0
 	else:
-		play_anim(false)
+		play_anim(0)
 		velocity.x = 0
 		velocity.y = 0
 	move_and_slide()
 
-func play_anim(movement : bool):
+func play_anim(movement : int):
 	var direction = current_direction
 	var anim = $AnimatedSprite2D
 	if direction == "right":
 		anim.flip_h = false
-		if movement:
+		if movement == 1:
 			anim.play("walk")
-		else:
-			anim.play("idle")
+		elif movement == 0:
+			if atk_ip == false:
+				anim.play("idle")
+
+				
 	elif direction == "left":
 		anim.flip_h = true
-		if movement: 
+		if movement == 1: 
 			anim.play("walk")
-		else:
-			anim.play("idle")
+		elif movement == 0:
+			if atk_ip == false:
+				anim.play("idle")
+
+				
 	
 
 
@@ -81,4 +89,25 @@ func  player():
 
 func _on_atk_cooldown_timeout() -> void:
 	enemy_atk_cd = true
+	pass # Replace with function body.
+
+func attack():
+	var dir = current_direction
+	if Input.is_action_just_pressed("Attack"):
+		Global.player_current_atk = true
+		atk_ip = true
+		if dir == "right":
+			$AnimatedSprite2D.flip_h = false
+		if dir == "left":
+			$AnimatedSprite2D.flip_h = true
+		$AnimatedSprite2D.play("sword_attack")
+		$DealATKTimer.start()
+			
+	
+
+
+func _on_deal_atk_timer_timeout() -> void:
+	$DealATKTimer.stop()
+	Global.player_current_atk = false
+	atk_ip = false
 	pass # Replace with function body.
